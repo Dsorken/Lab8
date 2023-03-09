feda96a98d89d070b08ddf0b1120997bfc75d843
Dsorken <113465018+Dsorken@users.noreply.github.com>
2023-03-08 18:29:16 -0700

    Update CustomListTest.java

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -66,7 +66,7 @@ public class CustomListTest {
         assertThrows( IllegalArgumentException.class, () -> {
             cityList.delete(city); });
     }
-
+    /*
     @Test
     void testCountCities() {
         CustomList cityList = MockCityList();
@@ -74,5 +74,5 @@ public class CustomListTest {
         cityList.add(city);
         assertEquals(2, cityList.countCities());
     }
-
+    */
 }
